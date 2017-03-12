a008666460aab103fd5f3f0304e390aadb7a35f4
jimmy <807065717@qq.com>
2017-03-12 10:42:05 +0800

    SD card is ok

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Mar 12 08:57:18 HKT 2017"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Mar 12 10:34:31 HKT 2017"
 #define LINUX_COMPILE_BY "jimmy"
 #define LINUX_COMPILE_HOST "jimmy"
 #define LINUX_COMPILER "gcc version 4.9.3 20150303 (release) [ARM/embedded-4_9-branch revision 221220] (GNU Tools for ARM Embedded Processors) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.4.0-00003-g38eeb0b-dirty"
+#define UTS_RELEASE "4.4.0-00004-gfd2437c-dirty"
